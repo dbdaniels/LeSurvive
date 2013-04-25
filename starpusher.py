97e65cdaf6c13cb612779974ed50a1c797c6119c
Derek Daniels <derek.daniels14@gmail.com>
2013-04-25 02:25:53 -0400

    Start of the menu

--- starpusher.py
+++ starpusher.py
@@ -5,6 +5,7 @@
 # Released under a "Simplified BSD" lice
 import random, sys, copy, os, pygame
 from pygame.locals import *
+from menu import *
 
 FPS = 1 # frames per second to update the screen
 WINWIDTH = 800 # width of the program's window, in pixels
@@ -38,16 +39,18 @@ LEFT = 'left'
 RIGHT = 'right'
 # -grayson: created lookDir for the player for direction management and added inventory system
 lookDir = 'forward'
-
+goToMenu = True
 def main():
     global FPSCLOCK, DISPLAYSURF, IMAGESDICT, \
            TILEMAPPING, OUTSIDEDECOMAPPING, BASICFONT, \
            PLAYERIMAGES, currentImage, lookDir, \
-           playerObj, cameraOffsetY, cameraOffsetX
+           playerObj, cameraOffsetY, cameraOffsetX,\
+           goToMenu
 
     # Pygame initialization and basic set up of the global variables.
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
+    
 
     # Because the Surface object stored in DISPLAYSURF was returned
     # from the pygame.display.set_mode() function, this is the
@@ -106,7 +109,10 @@ def main():
     playerObj = {'lookDir': lookDir,
                  'x': 0,
                  'y': 0}
-
+    if goToMenu:
+        menuScreen()
+        goToMenu = False
+    
     startScreen() # show the title screen until the user presses a key
 
     # Read in the levels from the text file. See the readLevelsFile() for
@@ -666,7 +672,74 @@ def centerCamera( mapWidth, mapHeight, gameStateObj):
     cameraOffsetX = (mapWidth/2) - ((plx * TILEWIDTH)+(TILEWIDTH/2))
     cameraOffsetY = (mapHeight/2) - ((ply * 40)+40)
     
+def menuScreen():
 
+
+   # Create a window of 800x600 pixels
+   screen = DISPLAYSURF
+
+
+   # Create 3 diffrent menus.  One of them is only text, another one is only
+   # images, and a third is -gasp- a mix of images and text buttons!  To
+   # understand the input factors, see the menu file
+   menu = cMenu(50, 50, 20, 5, 'vertical', 100, screen,
+               [('Start Game', 1, None),
+                ('Exit',       4, None)])
+
+   menu.set_center(True, True)
+
+   menu.set_alignment('center', 'center')
+
+   state = 0
+   prev_state = 1
+
+   # rect_list is the list of pygame.Rect's that will tell pygame where to
+   # update the screen (there is no point in updating the entire screen if only
+   # a small portion of it changed!)
+   rect_list = []
+
+   # Ignore mouse motion (greatly reduces resources when not needed)
+   pygame.event.set_blocked(pygame.MOUSEMOTION)
+
+   # The main while loop
+   while 1:
+      # Check if the state has changed, if it has, then post a user event to
+      # the queue to force the menu to be shown at least once
+      if prev_state != state:
+         pygame.event.post(pygame.event.Event(EVENT_CHANGE_STATE, key = 0))
+         prev_state = state
+
+      # Get the next event
+      e = pygame.event.wait()
+
+      # Update the menu, based on which "state" we are in - When using the menu
+      # in a more complex program, definitely make the states global variables
+      # so that you can refer to them by a name
+      if e.type == pygame.KEYDOWN or e.type == EVENT_CHANGE_STATE:
+         if state == 0:
+            rect_list, state = menu.update(e, state)
+         elif state == 1:
+            print ('Start Game!')
+            startScreen()
+            state = 0
+         elif state == 2:
+            print ('Load Game!')
+            state = 0
+         elif state == 3:
+            print ('Options!')
+            state = 0
+         else:
+            print ('Exit!')
+            pygame.quit()
+            sys.exit()
+
+      # Quit if the user presses the exit button
+      if e.type == pygame.QUIT:
+         pygame.quit()
+         sys.exit()
+
+      # Update the screen
+      pygame.display.update(rect_list)
 
 
 if __name__ == '__main__':
